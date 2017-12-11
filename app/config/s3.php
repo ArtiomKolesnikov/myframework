371abf48bfacbc90d0c14e063215d7c791d606f1
ArtiomKolesnikov <77777green77777@gmail.com>
2017-12-11 19:51:02 +0300

    Загрузка в облако

--- app/config/s3.php
+++ app/config/s3.php
@@ -1,7 +1,10 @@
 <?php
 return [
-    'key' => 'AKIAITHFPRGWGOKAI2ZQ',
-    'secret' => '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',
+    'credentials' => [
+        'key' => 'AKIAITHFPRGWGOKAI2ZQ',
+        'secret' => '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',
+    ],
     'bucket' => 'myframework',
     'region' => 'eu-central-1',
+    'ACL'    => 'public-read'
 ];
--- app/views/Download/index.php
+++ app/views/Download/index.php
@@ -1,41 +1,34 @@
 <?php
 /*Amazon*/
+
+use Aws\S3\Exception\S3Exception;
 use Aws\S3\S3Client;
+
 require ROOT . '/vendor/autoload.php';
+
 $config = require APP . '/config/s3.php';
-$s3 = S3Client::factory([
-    'version' => 'latest',
-    'key' => $config['key'],
-    'secret' => $config['secret'],
-    'region' => $config['region']
-]);
 
 if(isset($_FILES['file']))
 {
     $file = $_FILES['file'];
     $name = $file['name'];
     $tmp_name = $file['tmp_name'];
-    $extension = strtolower(end(explode('.',$name)));
-    d($file);
-
-    $key = md5(uniqid());
-    $tmp_file_name = "{$key}.{$extension}";
-    $tmp_file_path = "files/{$tmp_file_name}";
-//    move_uploaded_file($tmp_name,$tmp_file_path);
-
-    try{
+    $s3 = new S3Client([
+        'version' => 'latest',
+        'region'  => $config['region'],
+        'credentials' => $config['credentials']
+    ]);
+    try {
         $s3->putObject([
             'Bucket' => $config['bucket'],
-            'Key' => $name,
-            'Body' => fopen($file['tmp_name'],'rb'),
-            'ACL' => 'public-read'
+            'Key'    => $name,
+            'Body'   => file_get_contents($tmp_name),
+            'ACL'    => $config['ACL'],
         ]);
-
-//        unlink($tmp_file_path);
-
-    }catch (\Aws\S3\Exception\S3Exception $e){
-        dd($e);
+    } catch (S3Exception $e) {
+        echo "There was an error uploading the file.\n";
     }
+
 
 }
 ?>
